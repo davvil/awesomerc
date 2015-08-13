c5a3976b4e76bf0d727f5e848a26b6161e923e86
David Vilar <david.vilar@dfki.de>
2015-08-13 16:37:43 +0200

    Main menu also with middle mouse button on root window (i.e. right one on keyboard ;-))

--- rc.lua
+++ rc.lua
@@ -363,6 +363,7 @@ end
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
+    awful.button({ }, 2, function () mymainmenu:toggle() end),
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
